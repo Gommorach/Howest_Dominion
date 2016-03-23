60cfd77d1e45a043ae006b066a645641e4f34d1d
jeffreyDM <Jeffrey.de.maesschalck@student.howest.be>
2016-03-23 12:37:52 +0100

    player
    players

--- src/mainPackage/Player.java
+++ src/mainPackage/Player.java
@@ -1,5 +1,59 @@
 package mainPackage;
 
 public class Player {
-
+	
+	private int id;
+	private String name;
+	private int turn;
+	private int handSize;
+	private int actions;
+	private int buys;
+	private int coins;
+	private int victorypoints;
+	
+	
+	public Player(int id, String name, int turn, int handSize, int actions, int buys, int coins, int victorypoints){
+		this.id = id;
+		this.name = name;
+		this.turn = turn;
+		this.handSize = handSize;
+		this.actions = actions;
+		this.buys = buys;
+		this.coins = coins;
+		this.victorypoints = victorypoints;
+	}
+	
+	public int getPlayerid(){
+		return id;
+	}
+	
+	public String getname(){
+		return name;
+	}
+	
+	public int getturn(){
+		return turn;
+	}
+	
+	public int gethandSize(){
+		return handSize;
+	}
+	
+	public int actions(){
+		return actions;
+	}
+	
+	public int buys(){
+		return buys;
+	}
+	
+	public int coins(){
+		return coins;
+	}
+	
+	public int victorypoints(){
+		return victorypoints;
+	}		
 }
+
+
